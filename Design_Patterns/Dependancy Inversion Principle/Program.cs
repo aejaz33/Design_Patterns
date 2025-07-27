ed35aa48d14391c7db41c5fad4efd7f090e85fec
Aejaz Ahmad <aejaz.ahmad@connectwise.com>
2025-07-27 23:49:09 +0530

    Dependancy Inversion Principle Demo

---
+++ Design_Patterns/Dependancy Inversion Principle/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Dependancy_Inversion_Principle
+{
+    public enum Relation
+    {
+        Father,
+        Child,
+        Mother,
+    }
+    public class Person
+    {
+        public string name { get; set; }
+    }
+
+    public interface IRelation
+    {
+        IEnumerable<Person> GetAllChildrenOf(string name);
+    }
+
+    public class RelationShip : IRelation
+    {
+        public List<(Person, Relation, Person)> relations = new();//new List<(Person, Relation, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relation.Father, child));
+            relations.Add((child, Relation.Child, parent));
+        }
+        public IEnumerable<Person> GetAllChildrenOf(string name)
+        {
+            return relations.Where(p => p.Item1.name == name && p.Item2 == Relation.Father).Select(i => i.Item3);
+        }
+    }
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var parent = new Person { name = "Parent" };
+        //    var child1 = new Person { name = "Child1" };
+        //    var child2 = new Person { name = "Child2" };
+
+        //    var relationship = new RelationShip();
+        //    relationship.AddParentAndChild(parent, child1);
+        //    relationship.AddParentAndChild(parent, child2);
+
+        //    var childrenNamas = relationship.GetAllChildrenOf("Parent");
+        //    foreach (var child in childrenNamas)
+        //    {
+        //        Console.WriteLine($"Child Name: {child.name}");
+        //    }
+        //}
+    }
+}
---
+++ Design_Patterns/Dependancy Inversion Principle/Program2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependancy_Inversion_Principle
+{
+    /// <summary>
+    /// This is the Demo for Dependancy Inversion Principle which says high level class must not depend on low level class
+    /// Example: ILogger has different flavors of implementation it can have more flavors and DatabaseLogger does not changes becuase of change different loggings.
+    /// </summary>
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: Saved {message} in the file...");
+        }
+    }
+
+    public class DatabaseLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: Saved {message} in the database...");
+        }
+    }
+    internal class DataBaseLayer
+    {
+        private ILogger logger;
+        public DataBaseLayer(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void AddStudent(string name)
+        {
+            Console.WriteLine($"Saved {name} in the database");
+            logger.Log(name);
+        }
+    }
+    public class Program1
+    {
+        public static void Main(string[] args)
+        {
+            ILogger logger1 = new FileLogger();
+            ILogger logger2 = new DatabaseLogger();
+
+            var dac1 = new DataBaseLayer(logger1);
+            var dac2 = new DataBaseLayer(logger2);
+
+            dac1.AddStudent("Student1");
+            dac2.AddStudent("Student2");
+        }
+    }
+}
