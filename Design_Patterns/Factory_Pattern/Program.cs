9d4143f0fcfd1637956f68d3191256ea28fc7bb4
Aejaz Ahmad <aejaz.ahmad@connectwise.com>
2025-08-05 20:03:20 +0530

    Factory Pattern Demo

---
+++ Design_Patterns/Factory_Pattern/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Factory_Pattern
+{
+    /// <summary>
+    /// this is demonstration of Factory pattern where dedicates the responsiblity of creating the objects to Factory class or method.
+    /// We made constructor private to avoid new keyword in the code, so that no one can create objects
+    /// </summary>
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private Employee() { }
+
+        private Employee(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public static class EmployeeFactory
+        {
+            public static Employee NewEmployee(int id, string name)
+            {
+                return new Employee(id, name);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Employee Name: {this.Name} Employee ID: {this.Id}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = Employee.EmployeeFactory.NewEmployee(1, "Aejaz");
+            Console.WriteLine(employee.ToString());
+        }
+    }
+}
